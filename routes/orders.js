7f1131ce745288564f4ad5fb707d298378beebf2
David Oster <paspa@hotmail.com>
2022-02-13 13:28:13 +0200

    Updated Orders list with customer and product details

--- routes/orders.js
+++ routes/orders.js
@@ -1,5 +1,7 @@
 const db = require('../models/index'); // equivalent mysql
 const TestOrder = db.sequelize.models.TestOrder; // Model TestOrder
+const TestCustomer = db.sequelize.models.TestCustomer;
+const TestProduct = db.sequelize.models.TestProduct;
 var express = require('express');
 var router = express.Router();
 
@@ -7,10 +9,29 @@ var router = express.Router();
 // http://localhost:4000/orders
 router.get('/', async function (req, res, next) {
     let orders = await TestOrder.findAll({ attributes: ['id', 'customerId', 'productId', 'quantity', 'totalPrice'] });
+    let ordersDTO = [];
+    for(let element of orders){
+        let customer = await TestCustomer.findByPk(element.customerId, { attributes: ['firstName', 'lastName'] });
+        let customerName = customer.firstName + " " + customer.lastName;
+        let product = await TestProduct.findByPk(element.productId, { attributes: ['name']});
+        ordersDTO.push({
+            id: element.id, 
+            customer: { 
+                id: element.customerId, 
+                name: customerName
+            },
+            product: {
+                id: element.productId,
+                name: product.name
+            },
+            quantity: element.quantity,
+            totalPrice: element.totalPrice
+        });    
+    };
     res.render('orders/list', {
         title: 'Express 002 - Orders Page',
         message: "Orders",
-        list: orders
+        list: ordersDTO
     });
 });
 
